e37461babee635f1c3f61bb91490632a7375daf8
Your Name <zhuchanyoon@gmail.com>
2017-10-06 17:57:06 -0500

    configures Yelp api

--- eatquik.py
+++ eatquik.py
@@ -7,15 +7,22 @@ For additional samples, visit the Alexa Skills Kit Getting Started guide at
 http://amzn.to/1LGWsLG
 """
 
+
+
 from __future__ import print_function
+
+import env
 import requests
 import json
-#import helpers.userAddress
+from yelpcall import query_api
 
+
+query_api('restaurant', '303 Wadsack Dr, Norman, Oklahoma')
 
 # --------------- Helpers that build all of the responses ----------------------
 #url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBOimD8S4Ifw8o1XBEEFO7YKylK9d0sSJk'
 #r = requests.get(url)
+
 userAddress = ''
 """
 'card': {
